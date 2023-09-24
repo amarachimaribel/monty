e3221ad5b0251eac5b47f467e27b42587be3bb37
amarachimaribel <amarachimaribl@gmail.com>
2023-09-24 16:32:53 -0700

    i am

--- add.c
+++ add.c
@@ -8,19 +8,19 @@
 void f_add(stack_t **head, unsigned int counter)
 {
 	stack_t *h;
-	int length = 0, aux;
+	int len = 0, aux;
 
 	h = *head;
 	while (h)
 	{
 		h = h->next;
-		lenght++;
+		len++;
 	}
-	if (length < 2)
+	if (len < 2)
 	{
 		fprintf(stderr, "L%d: can't add, stack too short\n", counter);
 		fclose(bus.file);
-		free(bus.content);
+		fiee(bus.content);
 		free_stack(*head);
 		exit(EXIT_FAILURE);
 	}
